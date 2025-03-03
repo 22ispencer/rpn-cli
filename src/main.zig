27ef7933b22f81bca324be03cd26828cdb0bc729
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-03-03 14:55:59 -0600

    styled prompt

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,119 @@
 const std = @import("std");
 
+const Color = struct {
+    value: enum(u4) {
+        black = 0,
+        red = 1,
+        green = 2,
+        yellow = 3,
+        blue = 4,
+        magenta = 5,
+        cyan = 6,
+        white = 7,
+        default = 9,
+    },
+    bright: bool,
+
+    pub const default: Color = .{
+        .value = .default,
+        .bright = false,
+    };
+};
+
+const Style = struct {
+    fg_color: Color,
+    bg_color: Color,
+    bold: bool,
+    underlined: bool,
+
+    pub const reset_str = "\x1b[22;24;39;49m";
+
+    pub fn init(fg_color: ?Color, bg_color: ?Color, bold: bool, underlined: bool) Style {
+        return Style{
+            .fg_color = fg_color orelse Color.default,
+            .bg_color = bg_color orelse Color.default,
+            .bold = bold,
+            .underlined = underlined,
+        };
+    }
+};
+
+const FormattedText = struct {
+    pub fn init(text: []const u8, style: Style, allocator: std.mem.Allocator) !std.ArrayList(u8) {
+        var styled_string = std.ArrayList(u8).init(allocator);
+        var writer = styled_string.writer();
+        const csi = "\x1b[";
+        try writer.writeAll(csi);
+        if (style.bold) {
+            try writer.writeAll("1;");
+        }
+        if (style.underlined) {
+            try writer.writeAll("4;");
+        }
+        if (style.fg_color.value != .default) {
+            try writer.print("{s}{d};", .{
+                if (style.fg_color.bright) "9" else "3",
+                @intFromEnum(style.fg_color.value),
+            });
+        }
+        if (style.bg_color.value != .default) {
+            try writer.print("{s}{d};", .{
+                if (style.bg_color.bright) "10" else "4",
+                @intFromEnum(style.bg_color.value),
+            });
+        }
+        try writer.writeByte('m');
+        try writer.writeAll(text);
+        try writer.writeAll(Style.reset_str);
+        return styled_string;
+    }
+};
+
+fn repl(alloc: std.mem.Allocator) !void {
+    const stdout = std.io.getStdOut();
+    const stdin = std.io.getStdIn();
+
+    // buffer input & output
+    var writer = stdout.writer();
+    var reader = stdin.reader();
+
+    const prompt = try FormattedText.init("> ", Style.init(.{ .bright = false, .value = .green }, null, true, false), alloc);
+    defer prompt.deinit();
+
+    var input = std.ArrayList(u8).init(alloc);
+    defer input.deinit();
+
+    try writer.writeAll(prompt.items);
+
+    reader.readUntilDelimiterArrayList(&input, '\n', 4096) catch |err| switch (err) {
+        error.EndOfStream => return,
+        else => return err,
+    };
+
+    try writer.print("{s}", .{input.items});
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var args_iter = try std.process.argsWithAllocator(alloc);
+    defer args_iter.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var args = std.ArrayList([:0]const u8).init(alloc);
+    defer args.deinit();
 
-    try bw.flush(); // don't forget to flush!
+    var arg = args_iter.next();
+    while (arg != null) : (arg = args_iter.next()) {
+        try args.append(arg.?);
+    }
+
+    const arg_count: usize = args.items.len - 1;
+
+    if (arg_count == 0) {
+        try repl(alloc);
+    }
 }
 
 test "simple test" {
